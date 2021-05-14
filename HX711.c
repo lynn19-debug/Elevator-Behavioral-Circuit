3a2e0e31b497372f561488e824306efd75225366
lynn19-debug <82357029+lynn19-debug@users.noreply.github.com>
2021-05-14 16:55:11 -0700

    Update HX711.c

--- HX711.c
+++ HX711.c
@@ -1,17 +1,22 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_gpio.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_ssi.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/ssi.h"
 #include "driverlib/uart.h"
-#include "inc/tm4c123gh6pm.h"
 #include "utils/uartstdio.h"
+
+//#include "printf.h"
 
 /*GND - GND
   DT - PD2 - SSI1Rx
@@ -33,17 +38,28 @@ void uart_Init(void)
 	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 	
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);                                    
-        GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_2);   
-	// GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_2); ?
+  GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_2);   
+	GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_2);  
 
-  	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+  UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
-  	IntMasterEnable();                                                             //enable processor interrupts
+  IntMasterEnable();                                                             //enable processor interrupts
 	
-  	IntEnable(INT_UART0);                                                          //enable the UART interrupt
+  IntEnable(INT_UART0);                                                          //enable the UART interrupt
 	
-  	UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);                          //only enable RX and TX interrupts
+  UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);                          //only enable RX and TX interrupts
 }
+
+uint32_t reset()
+{
+}
+
+uint32_t ready()
+{
+}
+
+
+
 
 // unsigned long hx_readMass()
 uint32_t ReadCount(void)
@@ -56,8 +72,8 @@ uint32_t ReadCount(void)
 	
 	Count = 0;
 	
-	while (GPIO_PORTD_RIS_R&0x04); // while(ADDO); wait until Data Line goes LOW?
-	// while(GPIO_PORTD_DATA_R&0x04);
+	// while (GPIO_PORTD_RIS_R&0x04); // while(ADDO); wait until Data Line goes LOW?
+	while(GPIO_PORTD_DATA_R&0x04);
 	
 	for (i = 0; i < 24; i++)
 	{
@@ -68,11 +84,11 @@ uint32_t ReadCount(void)
 		
 		GPIO_PORTD_DATA_R &= ~0x01; // ADSK = 0
 		
-		if (GPIO_PORTD_RIS_R&0x04)
+		if (GPIO_PORTD_DATA_R&0x04)
 		{
 			Count++;
 			// Count = Count << 1;
-			SysCtlDelay(13333);
+			// SysCtlDelay(13333);
 		}
 	}
 	
@@ -85,13 +101,17 @@ uint32_t ReadCount(void)
 	GPIO_PORTD_DATA_R &= ~0x01;
 	return (Count);
 	
-	// sprintf(str, "%d", Count); ?	
+	SSIConfigSetExpClk(SSI1_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_1, SSI_MODE_MASTER, 10000, 16); SSIEnable(SSI1_BASE);
+	SSIEnable(SSI1_BASE);
+	
+	// sprintf(str, "%d", Count);
+	// covert number to string. 	
 }
 
 int main(void)
 {
 	// initialize UART0
-        uart_Init();
+  uart_Init();
 	
 	ReadCount();
 	
@@ -103,3 +123,65 @@ int main(void)
 	{
 	}
 }
+
+//#include "TM4C123GH6PM.h"
+
+//void init_SSI1(void);
+//// void LTC1661Write(int chan, short d);
+
+//int main(void)
+//{
+//    int i;
+
+//    init_SSI1();
+//    
+//    for(;;)
+//    {
+//        for (i = 0; i < 1023; i++)
+//        {
+//            // LTC1661Write(0, i); /* write a sawtooth to channel A */
+//        }
+//    }
+//}
+
+//void LTC1661Write(int chan, short data)
+//{
+//    GPIOF->DATA &= ~0x04;       /* assert SS low */
+//    data = (data & 0x03FF) << 2; /* bit 1-0 unused */
+//    if (chan == 0)              /* add control code with channel number */
+//        data |= 0x9000;
+//    else
+//        data |= 0xA000;
+//    while((SSI1->SR & 2) == 0); /* wait until FIFO not full */
+//    SSI1->DR = data >> 8;       /* transmit high byte */
+//    while((SSI1->SR & 2) == 0); /* wait until FIFO not full */
+//    SSI1->DR = data & 0xFF;     /* transmit low byte */
+//    while(SSI1->SR & 0x10);     /* wait until transmit complete */
+//    GPIOF->DATA |= 0x04;        /* keep SS idle high */
+//}
+
+//void init_SSI1(void)
+//{
+//    SYSCTL->RCGCSSI |= 2;       /* enable clock to SSI1 */
+//    SYSCTL->RCGCGPIO |= 8;      /* enable clock to GPIOD for SSI1 */
+//    //SYSCTL->RCGCGPIO |= 0x20;   /* enable clock to GPIOF for slave select */
+
+//    /* configure PORTD 2, 0 for SSI1 clock and Rx */
+//    GPIOD->AMSEL &= ~0x05;      /* disable analog for these pins */
+//    GPIOD->DEN |= 0x05;         /* and make them digital */
+//    GPIOD->AFSEL |= 0x05;       /* enable alternate function */
+//    GPIOD->PCTL &= ~0x0000F00F; /* assign pins to SSI1 */
+//    GPIOD->PCTL |= 0x00002002;  /* assign pins to SSI1 */
+//    	
+//    /* configure PORTF 2 for slave select */
+//    GPIOF->DEN |= 0x04;         /* make the pin digital */
+//    GPIOF->DIR |= 0x04;         /* make the pin output */
+//    GPIOF->DATA |= 0x04;        /* keep SS idle high */
+
+//    /* SPI Master, POL = 0, PHA = 0, clock = 4 MHz, 16 bit data */
+//    SSI1->CR1 = 0;          /* disable SSI and make it master */
+//    SSI1->CC = 0;           /* use system clock */
+//    SSI1->CPSR = 2;         /* prescaler divided by 2 */
+//    SSI1->CR0 = 0x0007;     /* 8 MHz SSI clock, SPI mode, 8 bit data */
+//    SSI1->CR1 |= 2;         /* enable SSI1 */
+//}
